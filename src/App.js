231720a175ec65bcb4ecbbce78630015467d137c
Tyler Schmidt <schmidt197@gmail.com>
2017-10-09 16:21:41 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Header from './components/header';
+
+
+
+class App extends Component {
+  render() {
+     return <Header />
+   }
+ }
+
+export default App;
