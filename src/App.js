1b5f3d6d68a60532c8af523f403c5770281db44d
Tyler Schmidt <schmidt197@gmail.com>
2017-10-11 16:23:34 -0700

    quick commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import Header from './components/header';
+import Nav from './components/nav';
 
 
 
 class App extends Component {
   render() {
-     return <Header />
+     return (
+       <div>
+         <Header />
+         <Nav />
+       </div>
+       )
    }
  }
 
