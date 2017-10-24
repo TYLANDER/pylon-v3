f0eca0b738e92400b0954ad77af23bb9724b3338
Tyler Schmidt <schmidt197@gmail.com>
2017-10-24 12:36:16 -0700

    fixed the nav animation!!

--- src/components/nav2.js
+++ src/components/nav2.js
@@ -2,22 +2,22 @@ import React from 'react'
 
 
 export default () => (
-<header class="w-100 z-max bg-white bb b--light-gray sans-serif">
-  <div class="mw7-m mw8-l center">
-    <div class="cf pa3">
-      <div class="fl w-100 w-20-ns pa0 ma0">
+<header className="headroom self-start w-100 accentcolorone z-max bg-white bb b--light-gray headroom--top headroom--not-bottom code">
+  <div className="mw7-m mw8-l center">
+    <div className="cf pa3">
+      <div className="fl w-100 w-20-ns pa0 ma0">
         <a href="/">
-          <ul class="list w-100 pa0 ma0">
-            <li class="dib mb0 mr2 mr4-l f4 f5-ns bg-dark-blue near-white pa1">Pylon</li>
+          <ul className="list w-100 pa0 ma0">
+            <li className="dib mb0 mr2 mr4-l f4 f5-ns bg-dark-blue near-white pa1">//]Pylon</li>
           </ul>
         </a>
       </div>
-      <div class="fl w-100 w-80-ns pl2-m pt3 pt0-ns tr-ns">
-        <ul class="list w-100 pa0 ma0 mt1">
+      <div className="fl w-100 w-80-ns pl2-ns pt3 pt0-ns tr-ns">
+        <ul className="list w-100 pa0 ma0 mt1">
 
-            <li class="dib mb1 mr2 mr4-l f5 f5-ns"><a class="ttc underline-hover link near-black no-underline" href="/about/">about</a></li>
+            <li className="dib mb1 mr3 mr4-l f6 f5-ns"><a className="ttc underline-hover link near-black no-underline" href="/about/">about</a></li>
 
-            <li class="dib mb1 mr2 mr4-l f5 f5-ns"><a class="ttc underline-hover link near-black no-underline" href="/work/">work</a></li>
+            <li className="dib mb1 mr3 mr4-l f6 f5-ns"><a className="ttc underline-hover link near-black no-underline" href="/work/">work</a></li>
 
         </ul>
       </div>
--- src/components/startuphero.js
+++ src/components/startuphero.js
@@ -9,7 +9,7 @@ const divStyle = {
 };
 
 export default () => (
-<header className='sans-serif'>
+<header className='bb b--light-gray sans-serif'>
   <div className='cover bg-center bg-top vh-75 vh-90-l' style={divStyle}>
     <div className='pb5 pb6-m pb7-l pt2 ph2' >
       <div className="tc mt0 mt4-ns ph4 ph5-m ph6-l content-center">
@@ -18,14 +18,3 @@ export default () => (
   </div>
 </header>
 )
-
-
-/* Headline text removed for testing hero images
-
-  <h1 className="f2 f1-l fw2 black-90 mb0 pt6 lh-title">Product Design and Development</h1>
-  <h2 class="fw1 f3 black-80 mt3 mb4">This site could be fancy, but really its just a sign. Email me bruh.</h2>
-  <a className="f6 no-underline grow dib v-mid bg-blue white ba b--blue ph3 pv2 mb3" href="/">Email Pylon</a>
-  <span className="dib v-mid ph3 white-70 mb3">or</span>
-  <a className="f6 no-underline grow dib v-mid white ba b--white ph3 pv2 mb3" href="http://thecatapi.com/api/images/get?format=src&type=gif">Look at a Cat</a>
-
- */
