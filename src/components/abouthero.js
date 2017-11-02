5bc793841755a071cb22bf361b50d1228b4d396b
Tyler Schmidt <schmidt197@gmail.com>
2017-11-01 18:24:50 -0700

    updated about and method title sections

--- src/components/abouthero.js
+++ src/components/abouthero.js
@@ -5,17 +5,17 @@ import React from 'react'
 export default () => (
 <div className="w-100">
 
-  <header className='mw8-m mw9-l center ph5-l pt6'>
-      <div className='cf pa3' >
-        <div className="fl w-50-l  mt0 mv5-ns mb5 fw4 f4 f3-ns v-mid-ns">
-          <h1 className="dib pr4-m pr6-ns f-5 fw6 bt bb bw2 mv0 mv3-m lh-title">Utility for End Users</h1>
-            <h4 className="f4 f4-ns lh-copy fw4 black-50 mt0 mv3 near-black measure-narrow">
-              Pylon is a global design and innovation consultancy based in San Francisco. Design is the way we add value to the world.
-            </h4>
-            <h4 className="f4 lh-copy fw4 black-50 mt0 mv3 near-black">We solve problems with teams of smart creatives.</h4>
-        </div>
-      <div class="fr w-50-l mt0 mb4 mb5-l v-mid-ns">
-          <img src={require('../images/about_hero@2x.png')} class="w-100 mw7 w6-ns o-85" alt="smileyface"/>
+  <header className='mw8-m mw9-l center ph3 ph5-l pt6'>
+    <div className='cf' >
+      <div className="fl dib w-100 w-50-l mt0-m mv5-ns mb5 fw4 f4 f3-ns v-mid-ns pr0 pr4-m">
+        <h1 className="dib  f-5 fw6 bt bb bw2 mv0 mv3-m lh-title">Utility for End Users</h1>
+          <h4 className="f4 f4-ns lh-copy fw4 mt0 mv3 near-black measure-narrow">
+            Pylon is a global design and innovation consultancy based in San Francisco. Design is the way we add value to the world.
+          </h4>
+          <h4 className="f4 lh-copy fw4 black-50 mt0 mv3 near-black">We solve problems with teams of smart creatives.</h4>
+      </div>
+      <div class="fr w-50-l mt0 mb3 mb5-l v-mid-ns">
+        <img src={require('../images/about_hero@2x.png')} class="w-100 mw7 w6-ns o-85" alt="smileyface"/>
       </div>
     </div>
   </header>
--- src/containers/AboutPage.js
+++ src/containers/AboutPage.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Nav2 from '../components/nav2'
-import MethodText from '../components/method-text'
-import DisplayBox from '../components/displaybox2col'
 import Footer3 from '../components/footer3'
 import PhilosophyText from '../components/philosophy-text'
 import TeamText from '../components/team-text'
@@ -13,9 +11,7 @@ export default () => (
   <div className='sans-serif'>
     <Nav2 />
     <AboutHero />
-    <DisplayBox />
     <PhilosophyText />
-    <MethodText />
     <TeamText />
     <Footer3 />
   </div>
