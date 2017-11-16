77e2250c2c2d39b359c81b8bd541d08741f2bcb3
Tyler Schmidt <schmidt197@gmail.com>
2017-11-15 16:51:19 -0800

    fixed alignment issue on about

--- src/components/about-tyler.js
+++ src/components/about-tyler.js
@@ -8,7 +8,7 @@ export default () => (
       <p className="fl w-100 w-25-ns db pa3 ph6-l mv3 mv4-ns f5 lh-copy">
         LEADERSHIP
       </p>
-        <img src={require('../images/tyler_profile_110317.png')} class="db w-90 w-30-ns ma3 ma4-ns shadow-4" alt="Tyler"></img>
+        <img src={require('../images/tyler_profile_110317.png')} class=" w-90 w-30-ns ma3 ma4-ns pl4-ns hadow-4" alt="Tyler"></img>
       <ul class="fr w-100 w-75-ns list tl">
         <li class="db pa3 pa4-ns">
           <b class="db f3 mb1">Tyler Schmidt</b>
@@ -16,8 +16,8 @@ export default () => (
             Tyler Schmidt has been a UX Design practicioner for the past 8 years. Now managing Pylon, Tyler manages a distributed team of best in class creatives to provide impactful solutions for modern brands. Tyler believes design is a way to add value in this world.
           </span>
         </li>
+        <Button />
       </ul>
     </section>
-    <Button />
   </article>
 )
